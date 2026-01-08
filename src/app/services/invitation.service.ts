33691785a2014f4e71137a96741e3f118598ac82
victor brito <victor-ebg@hotmail.com>
2026-01-08 17:49:36 -0400

    add new header

--- src/app/services/invitation.service.ts
+++ src/app/services/invitation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Invitation, InvitationResponse } from '../models/invitation.model';
@@ -10,19 +10,20 @@ import { environment } from '../../environments/environment';
 })
 export class InvitationService {
     private apiUrl = environment.apiUrl;
+    private headers = new HttpHeaders().set('ngrok-skip-browser-warning', 'true');
 
     constructor(private http: HttpClient) { }
 
     getInvitation(code: string): Observable<Invitation[]> {
-        return this.http.get<InvitationResponse>(`${this.apiUrl}/invitations?code=${code}`)
+        return this.http.get<InvitationResponse>(`${this.apiUrl}/invitations?code=${code}`, { headers: this.headers })
             .pipe(map(response => response.invitation ? [response.invitation] : []));
     }
 
     createInvitation(invitationData: any): Observable<any> {
-        return this.http.post(`${this.apiUrl}/invitations`, invitationData);
+        return this.http.post(`${this.apiUrl}/invitations`, invitationData, { headers: this.headers });
     }
 
     acceptInvitation(code: string): Observable<any> {
-        return this.http.post(`${this.apiUrl}/invitations/${code}/accept`, {});
+        return this.http.post(`${this.apiUrl}/invitations/${code}/accept`, {}, { headers: this.headers });
     }
 }
